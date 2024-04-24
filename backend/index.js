3dbe7e73821b5f7b71dfb758b60a9fa4abc23304
mohammaduamhar <mamamhar254@gmail.com>
2024-04-25 03:00:54 +0530

    heroko update

--- backend/index.js
+++ backend/index.js
@@ -4,21 +4,31 @@ import dotenv from "dotenv";
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import allRoutes from './routes/indexRoutes.js'
+import allRoutes from './routes/indexRoutes.js';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const PORT = process.env.PORT || 8000;
-
-
 
 const app = express();
 
 app.use(cors());
 app.use(morgan('tiny'));
 app.use(express.json());
-app.use(cookieParser());
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client", "build")));
+  app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.use(cookieParser());
 
 app.use('/api', allRoutes);
 
@@ -27,7 +37,6 @@ app.use((err, req, res, next) => {
   const message = err.message || 'Internal Server Error';
   return res.status(status).json({ message, stack: err.stack });
 });
-
 
 app.listen(PORT, () => {
   connectDB();
