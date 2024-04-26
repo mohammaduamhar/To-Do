664eb4b8f4ff1cde0f006222159e9f7210337417
mohammaduamhar <mamamhar254@gmail.com>
2024-04-26 23:34:18 +0530

    un commit

--- backend/index.js
+++ backend/index.js
@@ -21,12 +21,12 @@ app.use(cors());
 app.use(morgan('tiny'));
 app.use(express.json());
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "client",)));
-//   app.get("*", (req, res) => {
-//       res.sendFile(path.join(__dirname, "client","index.html"));
-//   });
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client", "build")));
+  app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.use(cookieParser());
 
